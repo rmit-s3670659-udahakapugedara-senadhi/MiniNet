8295b6b0205ede04b2f1e74e68695c37c30ee09d
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2018-03-24 18:55:33 +1100

    Profile Class
    Profile Class with Functionality.

---
+++ Profile.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Profile extends User {
+	ArrayList<String> friends = new ArrayList();
+	String pname;
+	String displaypic;
+	String status;
+	
+	
+	public Profile(String name, int age) {
+		super(name, age);
+	}
+	
+	public Profile(String name, int age, String pname) {
+		super(name, age);
+		this.pname = pname;	
+	}
+	
+	
+	public void addFriends(String pname) {
+		friends.add(pname);	
+	}
+	
+	
+	public void removeFriends(String pname) {
+		friends.remove(pname);	
+	}
+	
+	public ArrayList<String> getFriendlist() {
+		
+		return friends;
+		
+	}
+	
+	public String getName() {
+		
+		return this.pname;
+		
+	}
+	
+	
+	public String printAllFriends() {
+		
+			String print = friends.toString();
+			return print;				
+				
+		}
+
+	
+	public boolean searchFriends(String pname) {
+		
+		if (friends.contains(pname)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+				
+	}
+	
+	
+	public void addStatus(String status) {
+		this.status = status;	
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void changeName(String pname) {
+		this.pname = pname;	
+	}
+	
+	
+	public void setDisplayPic(String dp) {
+		this.displaypic = dp;
+	}
+	
+	
+	
+
+  
+}
